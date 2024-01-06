7627f29149a9a44d47f7d303ef1ecb339cd3de9c
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-06 15:05:04 +1100

    Add files via upload

---
+++ supplier.java
@@ -0,0 +1,25 @@
+public class supplier {
+	
+	int idSupplier:
+	String nameSupplier;
+	long nbnSupplier;
+	String addressSupplier;
+	long phoneSupplier;
+
+
+	boolean verifySupplier(String recebSupplier){
+
+
+		if(recebSupplier.equals(name)){
+			return true;
+		} else {
+			return false;
+		}
+
+
+
+	};
+
+
+
+}
