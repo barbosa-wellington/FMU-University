503d0f094bd170845abd6ebdac00280cc69758f1
Barbosa <63185850+barbosa-wellington@users.noreply.github.com>
2024-01-09 21:43:16 +1100

    Add files via upload

---
+++ sells.java
@@ -0,0 +1,16 @@
+public class sells {
+
+
+	public int numOrder;
+	public String nameDoc_Lisence;
+	public int idProduct;
+	public static int Qtorder;
+	public float valueUnit;
+	public float totalValueOrder;
+	public String addressOrder;
+	public Date dateOrder;
+
+	
+
+
+}
